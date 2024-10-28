2a71c58cabb340610f0cbcc8fcdb3f8e7d04841b
Panini02 <fabian.aegerter@epfl.ch>
2024-10-28 15:15:30 +0100

    Integrated the perpheral into x-heep

--- obi_spi_slave.sv
+++ obi_spi_slave.sv
@@ -8,7 +8,7 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-module axi_spi_slave #(
+module obi_spi_slave #(
 
     parameter OBI_ADDR_WIDTH = 32,
     parameter OBI_DATA_WIDTH = 32,

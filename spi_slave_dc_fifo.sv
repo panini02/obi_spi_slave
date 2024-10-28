503ce5d16dd1c0a6830ea4c88610e03f215edbf6
Panini02 <fabian.aegerter@epfl.ch>
2024-10-28 12:49:30 +0100

    Changed names in the axi_spi_slave file and changes due to running hello-world

--- spi_slave_dc_fifo.sv
+++ spi_slave_dc_fifo.sv
@@ -9,9 +9,9 @@
 // specific language governing permissions and limitations under the License.
 
 module spi_slave_dc_fifo #(
-    parameter DATA_WIDTH = 32,
+    parameter DATA_WIDTH   = 32,
     parameter BUFFER_DEPTH = 8
-)(
+) (
     input logic clk_a,
     input logic rstn_a,
     input logic [DATA_WIDTH-1:0] data_a,
@@ -24,22 +24,27 @@ module spi_slave_dc_fifo #(
     input logic ready_b
 );
 
-    typedef logic [DATA_WIDTH-1:0] data_t;
+  typedef logic [DATA_WIDTH-1:0] data_t;
 
-    cdc_fifo_gray #(.WIDTH(32), .T(data_t), .LOG_DEPTH(4), .SYNC_STAGES(2)) i_cdc_fifo_gray (
+  cdc_fifo_gray #(
+      .WIDTH(32),
+      .T(data_t),
+      .LOG_DEPTH(4),
+      .SYNC_STAGES(2)
+  ) i_cdc_fifo_gray (
 
-        .src_rst_ni (rstn_a ),
-        .src_clk_i  (clk_a ),
-        .src_data_i (data_a ),
-        .src_valid_i(valid_a),
-        .src_ready_o(ready_a),
+      .src_rst_ni (rstn_a),
+      .src_clk_i  (clk_a),
+      .src_data_i (data_a),
+      .src_valid_i(valid_a),
+      .src_ready_o(ready_a),
 
-        .dst_rst_ni (rstn_b ),
-        .dst_clk_i  (clk_b  ),
-        .dst_data_o (data_b ),
-        .dst_valid_o(valid_b),
-        .dst_ready_i(ready_b)
+      .dst_rst_ni (rstn_b),
+      .dst_clk_i  (clk_b),
+      .dst_data_o (data_b),
+      .dst_valid_o(valid_b),
+      .dst_ready_i(ready_b)
 
-    );
+  );
 
 endmodule
